f365700793abdcc7eeee6fb503c2c0e597d79925
darshan009 <darshansapaliga009@gmail.com>
2016-11-02 22:03:21 -0700

    updated product bidding

--- Back-End/controllers/user.js
+++ Back-End/controllers/user.js
@@ -411,25 +411,25 @@ function biddingTimeExpired() {
 };
 
 //repeated executing function
-// (function bidTimer() {
-//   var expiryDate = new Date();
-//   Advertisement.find()
-//     .exec(function(err, advertisements){
-//       if(err)
-//         console.log(err);
-//       for(var i=0; i<advertisements.length; i++){
-//         if(advertisements[i].biddingStartTime) {
-//           expiryDate.setDate(advertisements[i].biddingStartTime.getDate() + 4);
-//           if(expiryDate < advertisements[i].biddingStartTime)
-//             biddingTimeExpired(advertisements[i]._id);
-//         }
-//       }
-//       console.log("bidtimer");
-//
-//       //repeat this function again
-//       setTimeout(bidTimer(), 50000000);
-//     });
-// })();
+(function bidTimer() {
+  var expiryDate = new Date();
+  Advertisement.find()
+    .exec(function(err, advertisements){
+      if(err)
+        console.log(err);
+      for(var i=0; i<advertisements.length; i++){
+        if(advertisements[i].biddingStartTime) {
+          expiryDate.setDate(advertisements[i].biddingStartTime.getDate() + 4);
+          if(expiryDate < advertisements[i].biddingStartTime)
+            biddingTimeExpired(advertisements[i]._id);
+        }
+      }
+      console.log("bid timer");
+
+      //repeat this function again
+      setTimeout(bidTimer(), 5000);
+    });
+})();
 
 
 
@@ -452,6 +452,6 @@ exports.userLogs = function(msg, callback){
     if(err)
       console.log(err);
   });
-  
+
   callback(null, userLogs);
 };
